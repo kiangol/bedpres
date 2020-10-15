d971fa0ce46416a3d908381c1d1b8274466bab28
kiang <kiang@uio.no>
2020-10-15 04:02:45 +0200

    added headless

--- bedpresheadless.py
+++ bedpresheadless.py
@@ -3,18 +3,29 @@ import urllib.error
 from bs4 import BeautifulSoup
 from time import sleep
 from datetime import datetime
+from private import pushUrl
 
 # URL for the event
-url = "https://ifinavet.no/event/325"
+url = "https://ifinavet.no/event/"
+eventId = "325"
+# eventId = str(input("Enter event id (3 digits): "))
+url += eventId
+# rate = input("Refresh rate in seconds: ")
+rate = 30
 
 # Variables
-debug = True
 sendNotification = True
 loop = True
-refresh_rate = 30 # check every x seconds
+if rate:
+    rate = int(rate)
+else:
+    rate = 30
+refresh_rate = rate # check every x seconds
 
 # Use Pushcut app for notifications
-pushUrl = "https://api.pushcut.io/g6V-0NfXxpldSJqfvMgTK/notifications/Bedpres"
+# Modify URL to own notification URL 
+# pushUrl = "https://api.pushcut.io/*****/notifications/******"
+
 try:
     urlopen(pushUrl)
 except:
@@ -22,7 +33,6 @@ except:
 
 
 # Get initial value
-
 try:
     html = urlopen(url)
 except urllib.error.HTTPError as e:
@@ -30,7 +40,7 @@ except urllib.error.HTTPError as e:
     print(e)
     exit()
 
-bsObj = BeautifulSoup(html)
+bsObj = BeautifulSoup(html, features="html.parser")
 reqContainer = bsObj.find("div", {"class": "event-infobox"})
 
 contents = reqContainer.contents
@@ -40,7 +50,6 @@ initial = availBox.find("p").contents
 # Store initial value in this list
 valuesToCompare = []
 
-# Used to check for type (add string only)
 lastElement = initial[-1]
 # Add elements from result to comparision list
 print(" ----- INITIAL -----")
@@ -51,19 +60,21 @@ print(valuesToCompare)
 
 
 print(" ----- BEGIN -----")
-counter = 0
 while loop:
-    if counter % 10 == 0 and debug:
-        urlopen(pushUrl)
     freshValues = []
-    html = urlopen(url)
-    bsObj = BeautifulSoup(html)
-    reqContainer = bsObj.find("div", {"class": "event-infobox"})
+    try:
+        html = urlopen(url)
+        bsObj = BeautifulSoup(html, features="html.parser")
+        reqContainer = bsObj.find("div", {"class": "event-infobox"})
 
-    contents = reqContainer.contents
-    availBox = contents[len(contents)-2]
+        contents = reqContainer.contents
+        availBox = contents[len(contents)-2]
 
-    curr = availBox.find("p").contents
+        curr = availBox.find("p").contents
+    except :
+        print("Error occured. Will try again next loop")
+        continue
+        
     for i in curr:
         if(type(lastElement) == type(i)):
             freshValues.append(i)
@@ -71,7 +82,7 @@ while loop:
     now = datetime.now()
 
     # dd/mm/YY H:M:S
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")   
     time_string = now.strftime("%H:%M:%S")
     if(freshValues == valuesToCompare):
         print("Checked:", dt_string)
